8e36332dd78fdec3d0f1e52ca511afa0f92d89df
Wilson <whuang3078@gmail.com>
2021-11-06 21:59:06 -0400

    Finished first iteration

---
+++ game.js
@@ -0,0 +1,95 @@
+function computerPlay() {
+    let num = Math.floor(Math.random() * 3) + 1;
+    if (num === 1) {
+        return 'Rock';
+    } else if (num === 2) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    if (typeof playerSelection === 'string') {
+
+        let formattedPlayer = playerSelection.charAt(0).toUpperCase() 
+                            + playerSelection.slice(1).toLowerCase();
+
+        if (formattedPlayer === 'Rock' || formattedPlayer === 'Paper'
+        || formattedPlayer === 'Scissors') {
+            if (formattedPlayer === computerSelection) {
+                return 'Tie.';
+            } else if (formattedPlayer === 'Rock' && computerSelection === 'Paper') {
+                return 'You Lose.';
+            } else if (formattedPlayer === 'Rock' && computerSelection === 'Scissors') {
+                return 'You Win.';
+            } else if (formattedPlayer === 'Paper' && computerSelection === 'Rock') {
+                return 'You Win.';
+            } else if (formattedPlayer === 'Paper' && computerSelection === 'Scissors') {
+                return 'You Lose.';
+            } else if (formattedPlayer === 'Scissors' && computerSelection === 'Rock') {
+                return 'You Lose.';
+            } else if (formattedPlayer === 'Scissors' && computerSelection === 'Paper') {
+                return 'You Win.';
+            } 
+        }
+    }
+
+    return 'Please input "rock", "paper", or "scissors".';
+}
+
+let playerSelection = 'rock';
+console.log(playerSelection);
+const computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+playerSelection = 21;
+console.log(playRound(playerSelection, computerSelection));
+
+playerSelection = 'rOCk';
+console.log(playRound(playerSelection, computerSelection));
+
+playerSelection = 'rO';
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let playerSelection;
+    let result;
+    let i = 0;
+
+    while (i < 5) {
+        playerSelection = window.prompt('Rock, paper, scissors... shoot!')
+        result = playRound(playerSelection, computerPlay());
+        
+        if (result.slice(0, 6) !== 'Please') {
+            if (result === 'You Win.') {
+                playerScore++;
+                i++;
+            } else if (result === 'You Lose.') {
+                computerScore++;
+                i++;
+            } else {
+                playerScore++;
+                computerScore++;
+                i++;
+            }
+
+            console.log(result + ' Your score is ' + playerScore 
+                        + ' and the computer\'s score is ' + computerScore);
+        }
+    }
+
+    if (playerScore === computerScore) {
+        console.log('Tie game! What are the chances of that??');
+    } else if (playerScore < computerScore) {
+        console.log('The computer wins overall!');
+    } else {
+        console.log('You win overall!!!');
+    }
+}
+
+// game();
